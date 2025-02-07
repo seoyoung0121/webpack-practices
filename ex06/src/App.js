ae8c7ee184a4b968d9263697af2fb4be69078104
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-02-07 15:33:36 +0900

    webpack 기초

--- ex06/src/App.js
+++ ex06/src/App.js
@@ -1,12 +1,13 @@
-import './assets/scss/App.scss';
-import logo from './assets/images/logo.svg';
+import "./assets/scss/App.scss";
+import logo from "./assets/images/logo.svg";
 
 function App() {
-    const App = document.createElement('div');
-    App.textContent = "Hello World";
-    App.className = "Header";
-    App.innerHTML = `<img src='${logo}'>`;
-    return App;
+  console.log(logo);
+  const App = document.createElement("div");
+  App.textContent = "Hello World";
+  App.className = "Header";
+  App.innerHTML = `<img src='${logo}'>`;
+  return App;
 }
 
-export {App};
+export { App };
